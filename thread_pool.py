e2113ff8dca4dc564c15103e9e96e67d01252fd4
Xiomara <email@gmail.com>
2025-11-18 22:47:35 -0500

    Uso de ThreadPool

---
+++ thread_pool.py
@@ -0,0 +1,80 @@
+"""
+Taller 4 - Patrones Concurrentes
+Autor: Angie Xiomara Fuentes Montañez
+Fecha: (15/11/2025)
+"""
+import time, random, queue
+from concurrent.futures import ThreadPoolExecutor
+import threading
+
+
+lock = threading.Lock()
+
+clientes = ["Maria", "Leo", "Pablo", "Isabela"]
+cola = queue.Queue(maxsize=5)
+cant = {"producido": 0, "consumido": 0}
+
+
+def crear_pedido(id_prod, num):
+
+    return {
+        "id_pedido": f"P{id_prod}-{num}",
+        "cliente": random.choice(clientes),
+        "precio": round(random.uniform(700, 3000), 2),
+        "producto": random.choice(["Sandia", "Pera", "Manzana", "Uvas"])
+    }
+
+
+def productor(id_prod):
+
+    print(f"Iniciando el productor {id_prod}")
+
+    for i in range(3):
+        time.sleep(random.uniform(0.3, 1.2))
+        pedido = crear_pedido(id_prod, i)
+        cola.put(pedido)
+
+        with lock:
+            cant["producido"] += 1
+
+        print(f"[Productor {id_prod}] produjo: {pedido}")
+    print(f"[Productor {id_prod}] finalizó.")
+
+
+def consumidor(id_cons):
+
+    print(f"Iniciando el consumidor {id_cons}")
+
+    while True:
+        try:
+            pedido = cola.get(timeout=2)
+        except queue.Empty:
+            break
+
+        print(f"[Consumidor {id_cons}] procesando pedido {pedido['id_pedido']}")
+        time.sleep(random.uniform(0.5, 1.2))
+
+        with lock:
+            cant["consumido"] += 1
+
+        cola.task_done()
+
+    print(f"[Consumidor {id_cons}] finalizó.")
+
+
+def main():
+
+    inicio = time.time()
+
+    with ThreadPoolExecutor(max_workers=7) as executor:
+
+        for i in range(3):
+            executor.submit(productor, i)
+
+        for j in range(4):
+            executor.submit(consumidor, j)
+
+    fin = time.time()
+
+if __name__ == "__main__":
+    main()
