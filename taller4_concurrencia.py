fd7d46ed30db9ed02f0b22c5007eac3ce4dc7290
Xiomara <email@gmail.com>
2025-11-18 22:45:07 -0500

    Implementacion de produccion y consumo

---
+++ taller4_concurrencia.py
@@ -0,0 +1,71 @@
+"""
+Taller 4 - Patrones Concurrentes
+Autor: Angie Xiomara Fuentes Montañez
+Fecha: (18/11/2025)
+"""
+import threading, queue, time, random
+
+lock = threading.Lock()
+
+clientes = ["Maria", "Leo", "Pablo", "Isabela"]
+cola = queue.Queue(maxsize=5)
+cant = {"producido": 0, "consumido": 0}
+
+def crear_pedido(id_prod, num):
+
+    return{
+        "id_pedido": f"P{id_prod}-{num}",
+        "cliente": random.choice(clientes),
+        "precio": round(random.uniform(700, 3000), 2),
+        "producto": random.choice(["Sandia", "Pera", "Manzana", "Uvas"])
+    }
+
+def productor(id_prod):
+
+    print(f"Iniciando el productor {id_prod}")
+
+    for i in range(3):
+        time.sleep(random.uniform(0.3, 1.2))
+        pedido = crear_pedido(id_prod, i)
+        cola.put(pedido)
+
+
+        with lock:
+            cant["producido"] += 1
+
+        print(f"[Productor {id_prod}] produjo: {pedido}")
+    print(f"[Productor {id_prod}] finalizó.")
+
+def consumidor(id_cons):
+
+    print(f"Iniciando el consumidor {id_cons}")
+    while True:
+        try:
+            pedido = cola.get(timeout=2)
+        except queue.Empty:
+            break
+        print(f" [Consumidor {id_cons}] procesando pedido {pedido['id_pedido']}")
+        time.sleep(random.uniform(0.5, 1.2))
+        with lock:
+            cant["consumido"] += 1
+        cola.task_done()
+    print(f" [Consumidor {id_cons}] finalizó.")
+
+
+def main():
+
+    inicio = time.time()
+
+    productores = [threading.Thread(target=productor, args=(i,)) for i in range(3)]
+    consumidores = [threading.Thread(target=consumidor, args=(j,)) for j in range(4)]
+
+
+    for h in productores: h.start()
+    for h in consumidores: h.start()
+
+    for h in productores + consumidores: h.join()
+
+    fin = time.time()
+
+if __name__ == "__main__":
+    main()
